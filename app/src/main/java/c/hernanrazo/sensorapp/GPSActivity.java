fda1bc89871b1c78b10e9eb03c96bf3d20b1a118
Hernan Razo <hernanrazo@gmail.com>
2018-12-21 16:52:56 -0600

    added README.md

--- app/src/main/java/c/hernanrazo/sensorapp/GPSActivity.java
+++ app/src/main/java/c/hernanrazo/sensorapp/GPSActivity.java
@@ -8,7 +8,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class GPSActivity extends AppCompatActivity {
 
